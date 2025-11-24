31fe901565b71ea3cc8019bff25b68afc2815c10
shu-htk <shu-htk@outlook.jp>
2025-11-24 09:23:31 +0900

    modify script/make_html.sh

--- script/make_html.sh
+++ script/make_html.sh
@@ -4,13 +4,11 @@ css=border.css
 #css=test.css
 
 mkdir -p html/ex html/ref
-cat README.md | sed 's%doc/%html/%g' | sed 's%\.md%.html%g' > readme
+cat md/README.md | sed 's%doc/%html/%g' | sed 's%\.md%.html%g' > readme
 pandoc -f markdown -t html5 readme -s --self-contained -c $css -o index.html 
 rm readme
 
-#for f in doc/*.md doc/ex/*.md doc/ref/*.md ; do
-#for f in  doc/ref/logic.md ; do
-for f in  doc/install.md ; do
+for f in md/*.md md/ex/*.md md/ref/*.md ; do
     f2=$(basename ${f%.*})
     cat $f | sed 's%\.md%.html%g' > $f2
     dest=$(echo ${f%.*}.html | sed 's%doc/%html/%g')
