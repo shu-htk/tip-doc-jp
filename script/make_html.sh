0908566202a7630fc82c158bfc53bdaf0407dfbf
shu-htk <shu-htk@outlook.jp>
2025-11-26 23:22:57 +0900

    script/make_html.sh specify input files

--- script/make_html.sh
+++ script/make_html.sh
@@ -1,18 +1,21 @@
 #1/bin/bash
 
 css=border.css
-#css=test.css
+tmp=tmp/tmp
 
-mkdir -p html/ex html/ref
+mkdir -p tmp html/ex html/ref
 #cat md/README.md | sed 's%doc/%html/%g' | sed 's%\.md%.html%g' > readme
-#pandoc -f markdown -t html5 readme -s --self-contained -c $css -o index.html 
+#pandoc -f markdown -t html readme -s --embed-resources -c $css -o index.html 
 #rm readme
 
-for f in md/*.md md/ex/*.md md/ref/*.md ; do
-#for f in md/tutorial.md ; do
+#for f in md/*.md md/ex/*.md md/ref/*.md ; do
+for f in $* ; do
+    echo $f
     f2=$(basename ${f%.*})
     cat $f | sed 's%\.md%.html%g' > $f2
     dest=$(echo ${f%.*}.html | sed 's%md/%html/%g')
-    pandoc -f markdown -t html5 $f2 -s --self-contained -c $css -o $dest
+    pandoc -f markdown -t html $f2 -s --embed-resources -c $css -o $tmp
+    cat $tmp | sed 's|<pre><code>|<pre><code>\n|g' \
+	| sed 's|</code></pre>|\n </code></pre>|g' > $dest
     rm $f2
 done
